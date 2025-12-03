fb9d4c7ed401bcc9a3d0ccb8696d33a918892c49
Amulya-Billemane <msamulyabillemane@gmail.com>
2025-12-03 09:27:22 +0530

    Initial commit

---
+++ AddNumbers.java
@@ -0,0 +1,11 @@
+public class AddNumbers
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+
+        System.out.println("The sum is = ",sum);
+    }
+}
